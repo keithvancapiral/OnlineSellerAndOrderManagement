9b900953c817ee8708b3344a69dcff496c86d4ce
paulmercado <paulmercadoo43@gmail.com>
2017-04-24 11:40:05 +0800

    update string message

--- OnlineSellerAndOrderManagement/OnlineSellerAndOrderManagement/Program.cs
+++ OnlineSellerAndOrderManagement/OnlineSellerAndOrderManagement/Program.cs
@@ -11,7 +11,7 @@ namespace OnlineSellerAndOrderManagement
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World  mo to :)");
-            Console.WriteLine("Hello World  mo to sample :)");
+            Console.WriteLine("Hello World  mo to sample ko to :)");
             Console.Read();
         }
     }
