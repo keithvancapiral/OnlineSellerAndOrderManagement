2f9ad5b1ead1fa0fa2884d43d0cadbdc2f2a82e7
paulmercado <paulmercadoo43@gmail.com>
2017-04-24 11:35:47 +0800

    initialize project

--- OnlineSellerAndOrderManagement/OnlineSellerAndOrderManagement/Program.cs
+++ OnlineSellerAndOrderManagement/OnlineSellerAndOrderManagement/Program.cs
@@ -10,7 +10,7 @@ namespace OnlineSellerAndOrderManagement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World :)");
+            Console.WriteLine("Hello World  mo to :)");
             Console.Read();
         }
     }
